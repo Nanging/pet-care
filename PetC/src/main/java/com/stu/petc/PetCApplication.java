d7133d44ae9c7c2a208552fd2aea9bd1a563cb70
Nanging <q1006280595@163.com>
2018-11-23 11:57:52 +0800

    空白项目模板

---
+++ PetC/src/main/java/com/stu/petc/PetCApplication.java
@@ -0,0 +1,12 @@
+package com.stu.petc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCApplication.class, args);
+	}
+}

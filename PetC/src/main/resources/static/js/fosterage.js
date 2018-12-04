d0e5dcb639e184a4cd156276b228fd65a073cbe0
YGrylls <910357107@qq.com>
2018-12-04 16:06:02 +0800

    Photo upload added, detail page added

--- PetC/src/main/resources/static/js/fosterage.js
+++ PetC/src/main/resources/static/js/fosterage.js
@@ -13,8 +13,55 @@ $(document).ready(function () {
 
     });
 
+    $("#imgInput").fileinput({
+        uploadUrl:"/uploadImage",
+        msgFilesTooLess:"You should choose at least 1 image",
+        msgFilesTooMany:"You should choose no more than 3 images",
+        allowedFileExtensions:["jpg","jpeg","png"],
+        msgInvalidFileExtension:"You can only upload jpg, jpeg, png files",
+        minFileCount:1,
+        maxFileCount:3,
+        maxFileSize:3000,
+        uploadAsync: false,
+        msgSizeTooLarge:"The image should be no more than 3MB",
+        showClose:false,
+        showCancel:false,
+        showUpload: false,                              //http://plugins.krajee.com/file-input#ajax-sync  FOR HELP
+    }).on("filebatchuploadsuccess",function (event,data) {  //if images uploaded successfully, then post the publish request with the url of images
+        var imageList=data.response.imagelist;           // http://plugins.krajee.com/file-input/plugin-events#filebatchuploadsuccess   FOR HELP
+        var publishData=$("#publishForm").serializeArray();
+        var s={"images":[]};
+        $.each(publishData,function (index,field) {      //transfer form into json string
+            s[field.name] = field.value;
+        });
+        $.each(imageList,function (index,field) {         //add the image url into the publish
+            s["images"][index]=field;
+        })
+        console.log(s);
+        $.ajax({
+            url: "/publishFoster",  //tartget url
+            type: "POST",
+            contentType: "application/json;charset=utf-8",
+            dataType: 'json',
+            data: JSON.stringify(s),
+            success:function(result){
+                console.log(result);
+                //to be done
+            },
+            error:function (result) {
+                console.log(result);
+                //to be done
+            }
 
-    console.log( $("#titleCounter").text())
+        })
+    })
+
+    $("#imgInput").on("filebatchuploaderror",function(event,data,msg){ //upload failed
+        console.log(msg);                                          //http://plugins.krajee.com/file-input/plugin-events#filebatchuploaderror FOR HELP
+        //in design
+
+    });
+
 })
 
 function counterTitle() {
@@ -25,10 +72,29 @@ function counterDetail() {
     $("#detailCounter").text(""+$("#newContent").val().length+"/800");
 }
 
-$(".grid").imagesLoaded().progress(function () {
+$(".grid-item").imagesLoaded().progress(function () {
     $(".grid").masonry("layout");
 });
 
 function filterSubmit(){
     $("#searchForm").submit();
+}
+function inputCheck(){
+    if($("#newTitle").val()==''){
+        $("#errorText").append("<small style='color:orangered'><span class='glyphicon glyphicon-remove'></span>Title cannot be empty</small>");
+        return false;
+    }
+    if($("#newContent").val()==''){
+        $("#errorText").append("<small style='color:orangered'><span class='glyphicon glyphicon-remove'></span>Details cannot be empty</small>");
+        return false;
+    }
+}
+
+
+function publishSubmit(){
+    $("#errorText").children("small").remove();
+    if(!inputCheck()) return;
+    $("#imgInput").lock();
+    $("#imgInput").upload(); //upload images first
+    $("#imgInput").unlock();
 }

f69bec1aa8855e5dbbaa485e44aa628c9121fd94
Nanging <q1006280595@163.com>
2018-12-17 00:12:12 +0800

    userinfo show fix

--- PetC/src/main/java/com/stu/petc/controller/UserInfoController.java
+++ PetC/src/main/java/com/stu/petc/controller/UserInfoController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,7 +29,7 @@ public class UserInfoController {
 	UserInfoService service;
 	
 	@RequestMapping("/user/info")
-	public String userInfo(HttpServletRequest request,ModelAndView model) {
+	public String userInfo(HttpServletRequest request,Model map) {
 		String username = "";
 		Cookie[] cookies = request.getCookies();
 		if (null!=cookies) {
@@ -60,9 +61,9 @@ public class UserInfoController {
 			for (ShareNote shareNote : shareList) {
 				System.out.println(shareNote);
 			}
-			model.addObject("fosterList", fosterList);
-			model.addObject("shareList", shareList);
-			model.addObject("adoptionList", adoptionList);
+			map.addAttribute("fosterList", fosterList);
+			map.addAttribute("shareList", shareList);
+			map.addAttribute("adoptionList", adoptionList);
 			
 		}
 		return "userInfo";

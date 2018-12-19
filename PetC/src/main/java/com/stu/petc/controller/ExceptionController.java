0b4d883e1f8390a127a2b72db178abf46fd51bf4
Nanging <q1006280595@163.com>
2018-12-19 23:23:19 +0800

    /error delete

--- PetC/src/main/java/com/stu/petc/controller/ExceptionController.java
+++ PetC/src/main/java/com/stu/petc/controller/ExceptionController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ExceptionController {
 	
-	@RequestMapping("/error")
-	@ResponseBody
-	public String error() {
-		
-		return "error";
-	}
+//	@RequestMapping("/error")
+//	@ResponseBody
+//	public String error() {
+//		
+//		return "error";
+//	}
 	
 }

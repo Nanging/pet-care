143f99b4b6f0f6cce874bf6807a269e8c9f65b1b
Nanging <q1006280595@163.com>
2018-12-19 11:18:27 +0800

    intercepter url add

--- PetC/src/main/java/com/stu/petc/config/WebConfig.java
+++ PetC/src/main/java/com/stu/petc/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.stu.petc.config;
 
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -11,6 +15,18 @@ import com.stu.petc.web.LoginHandlerInterceptor;
 import com.stu.petc.web.UserHandlerInterceptor;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+
+	@Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                ErrorPage error404 = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+                ErrorPage error500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500");
+                factory.addErrorPages(error404,error500);
+            }
+        };
+    }
 
 	@Bean LoginHandlerInterceptor createLoginHandlerInterceptor() {
 		
@@ -35,7 +51,7 @@ public class WebConfig implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.addInterceptor(createLoginHandlerInterceptor()).addPathPatterns("/login");
-		registry.addInterceptor(createUserHandlerInterceptor()).addPathPatterns("/main");
+		registry.addInterceptor(createUserHandlerInterceptor()).addPathPatterns(new String[] {"/main","/foster/**","/share/**","/user/**"}).excludePathPatterns("/login");
 //		WebMvcConfigurer.super.addInterceptors(registry);
 	}
 
---
+++ PetC/src/main/java/com/stu/petc/controller/ExceptionController.java
@@ -0,0 +1,17 @@
+package com.stu.petc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ExceptionController {
+	
+	@RequestMapping("/error")
+	@ResponseBody
+	public String error() {
+		
+		return "error";
+	}
+	
+}

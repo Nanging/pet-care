09bf05c235f721fa6e9366d3fe221a9152a29109
Nanging <q1006280595@163.com>
2018-12-20 16:08:55 +0800

    register fix

--- PetC/src/main/java/com/stu/petc/controller/LoginController.java
+++ PetC/src/main/java/com/stu/petc/controller/LoginController.java
@@ -154,22 +154,24 @@ public class LoginController {
 						|| reqUser.getPhone().length() > 16 || reqUser.getPhone().isEmpty()) {
 					return new LoginResponse(-3, "WRONG ACCESS", null);
 				}
+				if (!Pattern.matches("^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\\\d{8}$", reqUser.getPhone())) {
+					System.out.println("Wrong phone number");
+					return new LoginResponse(-4, "WRONG PHONE PATTERN", null);
+				}
 				/**
 				 * add new user
 				 */
-//				user.setUsername(reqUser.getUsername());
-//				user.setPassword(reqUser.getPassword());
-//				user.setUser_tel(reqUser.getPhone());
-//				if (1==mapper.add(user)) {
-//					return new LoginResponse(0, "SUCCESS", null);
-//				}
+				user.setUsername(reqUser.getUsername());
+				user.setPassword(reqUser.getPassword());
+				user.setUser_tel(reqUser.getPhone());
+				if (1==mapper.add(user)) {
+					return new LoginResponse(0, "SUCCESS", null);
+				}
 				/**
 				 * re phone number
 				 */
-				if (Pattern.matches("1(([38]\\d)|(5[^4&&\\d])|(4[579])|(7[0135678]))\\d{8}", reqUser.getPhone())) {
-					System.out.println("Wrong phone number");
-				}
-				return new LoginResponse(0, "SUCCESS", null);
+
+//				return new LoginResponse(0, "SUCCESS", null);
 			} else {
 				return new LoginResponse(-1, "Username exists", null);
 			}

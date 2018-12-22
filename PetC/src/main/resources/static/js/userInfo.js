9f4cff13f8c026536b1d748aad580d17ed79453a
zw6666 <2714709357@qq.com>
2018-12-22 18:25:33 +0800

    confirm更新

--- PetC/src/main/resources/static/js/userInfo.js
+++ PetC/src/main/resources/static/js/userInfo.js
@@ -42,9 +42,28 @@ function deleteData(tar) {
 function confirm(tar) {
     alert("confirm");
     console.log("click Info");
-    var link=tar.getAttribute('data-detailTarget');
-    console.log(link);
-    $("#infoModal .modal-body").load(link,function () {
-        $("#infoModal").modal("show");
-    })
+    var person_id=tar.getAttribute('data-person_id');
+    var card_id=tar.getAttribute('data-card_id');
+    var s={};
+    s["person_id"]=person_id;
+    s["card_id"]=card_id;
+    var data=JSON.stringify(s);
+    console.log(data);
+    $.ajax({
+        url:confirmurl,
+        type:"POST",
+        contentType: "application/json;charset=utf-8",
+        dataType:'json',
+        data:data,
+        success:function (result) {
+            // 确认后返回一个路由加载确认后的页面
+            console.log(result);
+            $("#infoModal .modal-body").load(result,function () {
+                $("#infoModal").modal("show");
+            })
+        },
+        error:function () {
+            console.log("error");
+        }
+    });
 }

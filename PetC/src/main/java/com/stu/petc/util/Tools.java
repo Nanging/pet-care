9ca9da2f0809f7bcc335172bda157c05a054da44
biubiubiupiu <yangguangda555@163.com>
2018-12-25 20:00:10 +0800

    bbb

--- PetC/src/main/java/com/stu/petc/util/Tools.java
+++ PetC/src/main/java/com/stu/petc/util/Tools.java
@@ -22,6 +22,9 @@ public class Tools {
 	      }
 	      String imgDirectory=path.getParentFile().getParentFile().getParent() + "/img/";
 	      imgDirectory = imgDirectory.replace('\\', '/');
+	      if(imgDirectory.startsWith("file:/")) {
+	    	  imgDirectory = imgDirectory.replaceAll("file:/", "");
+	      }
 	      System.out.println(imgDirectory);
 	      return imgDirectory;
 	}
